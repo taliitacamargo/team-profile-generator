01156c969f1c49285395d8dc7c63c34f74ca9996
Tali <taliitacamargo@icloud.com>
2021-11-15 20:52:44 -0500

    adding tests

--- test/Manager.test.js
+++ test/Manager.test.js
@@ -1,1 +1,9 @@
-const Manager = require ('./lib/Manager');
+const Employee = require ('../lib/Employee');
+const Manager = require ('../lib/Manager');
+
+test ("Should return Manager role", () => {
+    const role =  "Manager";
+    const mgn = new Manager ("Foo", id, "email", office);
+    expect(mgn.getRole()).toBe(role)
+
+});
