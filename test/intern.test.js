3d3f22b5ddef6411df30b7f80639d5dab7186b17
Tali <taliitacamargo@icloud.com>
2021-11-23 15:19:38 -0500

    adding intern.js test

--- test/intern.test.js
+++ test/intern.test.js
@@ -1,2 +1,23 @@
-// const Intern = require ('../lib/Intern');
+const Intern = require ('../lib/Intern');
 
+
+
+
+test ("Should return Intern role", () => {
+
+    const role =  "Intern";
+    const int = new Intern ("Bob Hardek", 1, "testintern.gmail", "OSU");
+    expect(int.getRole()).toBe(role);
+
+});
+test("Can set School", () => {
+    const testNumber = "OSU";
+    const int = new Intern ("Bob Hardek", 1, "test.gmail", testNumber);
+    expect(int.school).toBe(testNumber);
+});
+test("Can get School", () => {
+    const testNumber = "OSU";
+    const int = new Intern ("Bob Hardek", 1, "test.gmail", testNumber);
+    expect(int.getSchool()).toBe(testNumber);
+
+});
